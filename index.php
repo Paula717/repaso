b8edc38643ec8fe4c796a25b126d30da8ce16cd5
Paula Cuellar <paulandrea96@udenar.edu.co>
2020-11-01 10:48:36 -0500

    se agregaron archivos de bootstrap

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Repaso Seminario </title>
+	<!-- Archivos de estilos --> 
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+</head>
+<body>
+	<div class="alert alert-primary" role="alert">
+	  	<h1> HOLA MUNDO  </h1>
+		<h2> Mi nombre es Paula </h2>
+	</div>
+
+	<!-- Archivos Javascript -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
